a23c32da65d777474ebe5a7f779a7d63018b7ea0
sanjanajeyagopi1 <SanjanaJ@docu3c.com>
2024-10-17 20:34:06 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+apt-get update && apt-get install -y \  
+    pandoc \  
+    texlive-latex-base \  
+    texlive-xetex \  
+    texlive-fonts-recommended \  
+    texlive-plain-generic \  
+    texlive-latex-extra \  
+    texlive-fonts-extra \  
+    texlive-science \  
+    texlive-pictures \  
+    texlive-pstricks  
